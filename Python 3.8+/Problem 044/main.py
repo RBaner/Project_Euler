2f64ca6edcd1d2c8df58ee7cdfbc50b01972c34c
Rohit B <rohitbanerje@gmail.com>
2022-03-08 01:52:17 -0500

    optimized p 44, python

--- Python 3.8+/Problem 044/main.py
+++ Python 3.8+/Problem 044/main.py
@@ -1,14 +1,8 @@
-#SLOW
-#There has to be a mathematical way to speed things up/constrict the possibilities
-#perhaps not worth it to store pent numbers and instead generate them as needed
+#Lesson learned: Never memoize what can be generated quickly
 import time
 from math import sqrt
 
-def increase_pentagonal_numbers(current_state: list,upper_bound: int) -> list:
-    while max(current_state) < upper_bound:
-        nextNumber = int(0.5 * (len(current_state)+1) * (3*(len(current_state)+1)-1))
-        current_state.append(nextNumber)
-    return(current_state)
+pent = lambda x: int(x * (3*x - 1)/2)
 
 def is_pent(x):
 	f = (.5 + sqrt(.25+6*x))/3
@@ -18,20 +12,13 @@ def is_pent(x):
 		return False
 
 def main() -> int:
-    pentagonalNumbers = [1,5]
-    pentagonalNumbers = increase_pentagonal_numbers(pentagonalNumbers,150)
-    for i in pentagonalNumbers:
-        #print(i)
-        if i == max(pentagonalNumbers):
-            pentagonalNumbers = increase_pentagonal_numbers(pentagonalNumbers,max(pentagonalNumbers)*2)
-            print(len(pentagonalNumbers))
-        for j in pentagonalNumbers[:pentagonalNumbers.index(i)]:
-            summ = i + j
-            diff = i - j
-            if summ >= max(pentagonalNumbers) or diff >= max(pentagonalNumbers):
-                pentagonalNumbers = increase_pentagonal_numbers(pentagonalNumbers,max(summ,diff) + 1)
-            if summ in pentagonalNumbers and diff in pentagonalNumbers:
-                return(abs(i-j))
+    current = 2
+    while True:
+        currentPent = pent(current)
+        for i in range(1,current):
+            if is_pent(currentPent - pent(i))  and is_pent(currentPent + pent(i)):
+                return(abs(currentPent - pent(i)))
+        current += 1
 
 if __name__=="__main__":
     start = time.time()
