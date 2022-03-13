efba657ca37e91409354f0b9755be94d23ea97e6
Rohit B <rohitbanerje@gmail.com>
2022-03-13 07:11:32 -0400

    cleaned up p48 python

--- Python 3.8+/Problem 048/main.py
+++ Python 3.8+/Problem 048/main.py
@@ -3,8 +3,8 @@ import time
 def last_ten_digits(n:int) -> int:
     output = 0
     for i in range(1,n+1):
-        output += (i**i)%(10**10)
-    return(output%(10**10))
+        output = (output + (i**i))%(10**10)
+    return(output)
 
 def main()->int:
     return(last_ten_digits(1000))
