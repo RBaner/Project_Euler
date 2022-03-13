0b1494d6a5b232f43fabc3eb42a32eea8373b51a
Rohit B <rohitbanerje@gmail.com>
2022-03-13 06:34:55 -0400

    p 48, python

---
+++ Python 3.8+/Problem 048/main.py
@@ -0,0 +1,16 @@
+import time
+
+def last_ten_digits(n:int) -> int:
+    output = 0
+    for i in range(1,n+1):
+        output += (i**i)%(10**10)
+    return(output%(10**10))
+
+def main()->int:
+    return(last_ten_digits(1000))
+
+if __name__=="__main__":
+    start = time.time()
+    print(main())
+    end = time.time()
+    print(end-start)
