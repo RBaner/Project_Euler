591088b0e99a265c97d5d3057188461bfe808aa4
Rohit B <rohitbanerje@gmail.com>
2022-03-16 02:32:48 -0400

    p47 python, space improvement

--- Python 3.8+/26-50/Problem 047/main.py
+++ Python 3.8+/26-50/Problem 047/main.py
@@ -1,16 +1,11 @@
-#A rotating window that holds the current numbers in view
-#avoids recording/recalculating any primefactors as well as 
-#the complexity of reorganizing to make it "in order"
 from sympy import primefactors
 import time
 
 def n_consec_n_distinct_prime_factors(n):
-    window = [primefactors(i) for i in range(2,n+2)]
     current = 2
     run = 0
     while True:
         current_factors = primefactors(current)
-        window[current%n] = current_factors
         if len(current_factors) == n:
             run +=1
             if run == n:
