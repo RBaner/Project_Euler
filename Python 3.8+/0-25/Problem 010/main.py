1ebfdcf52832ba293773d6446eca2cdf8e237efa
Rohit B <rohitbanerje@gmail.com>
2022-03-18 23:59:40 -0400

    p10 python

---
+++ Python 3.8+/0-25/Problem 010/main.py
@@ -0,0 +1,27 @@
+from time import time
+from math import sqrt
+from sympy import isprime
+
+# def isprime(n):
+#     if n%2 == 0:
+#         if n == 2:
+#             return(True)
+#         else:
+#             return(False)
+#     else:
+#         for i in range(3,int(sqrt(n))+1,2):
+#             if n%i == 0:
+#                 return(False)
+#         return(True)
+
+def main():
+    sum = 2
+    for i in range(3,2*(10**6)+1,2):
+        if isprime(i):
+            sum+=i
+    return(sum)
+
+if __name__=="__main__":
+    start = time()
+    print(main())
+    print(time()-start)
