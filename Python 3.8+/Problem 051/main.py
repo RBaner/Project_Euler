bf37b29e56d2fb3d612a445cb026ae9f72706b87
Rohit Banerjee <rohitbanerje@gmail.com>
2022-04-02 12:19:35 -0400

    p51 python, now working but not correct

--- Python 3.8+/Problem 051/main.py
+++ Python 3.8+/Problem 051/main.py
@@ -1,4 +1,4 @@
-#not working
+# working but incorrect
 
 from itertools import combinations
 from math import log10
@@ -8,20 +8,25 @@ def findPrimeFamilySeed(n)->int:
     found = False
     current_num = 1
     maximum = 0
-    while not found:
+    while True:
         for r in range(1,int(log10(current_num))+1):
             for indicies in combinations([i for i in range(int(log10(current_num))+1)],r):
-                current_num_string = str(current_num)
-                #calculate family size by taking length of list comprehension, allow elements to pass into list by 
-                #boolean isprime
-                current_maximum = len([])
-
-
+                if int(log10(current_num)) in indicies:
+                    break
+                run = 0
+                for digit in range((1 if 0 in indicies else 0),10):
+                    current_num_string = str(current_num)
+                    for i in indicies:
+                        current_num_string = current_num_string[:i] + str(digit) + current_num_string[i+1:]
+                    if isprime(int(current_num_string)):
+                        run += 1
+                if run == n:
+                    return(current_num)
         current_num += 1
 
 
 def main()->int:
-    return(findPrimeFamilySeed(8))
+    return(findPrimeFamilySeed(7))
 
     
 
