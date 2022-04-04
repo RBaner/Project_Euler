e8e23f02019a1dd98cd0a7b4727167e65844ea5b
Rohit B <rohitbanerje@gmail.com>
2022-04-04 13:00:38 -0400

    p56 python, updated

--- Python 3.8+/Problem 056/main.py
+++ Python 3.8+/Problem 056/main.py
@@ -1,11 +1,21 @@
 def digitSum(n):
     return(sum([int(i) for i in str(n)]))
 
-maximum=0
-for a in range(1,100):
-    for b in range(1,100):
-        summ = digitSum(a**b)
-        if summ > maximum:
-            maximum = summ
+def maxabDigitSum(n):
+    maximum=0
+    for a in range(1,n):
+        for b in range(1,n):
+            summ = digitSum(a**b)
+            if summ > maximum:
+                maximum = summ
+    return(maximum)
 
-print(maximum)
+def main()->int:
+    return(maxabDigitSum(100))
+
+if __name__=="__main__":
+    from time import time
+    start = time()
+    print(main())
+    end = time()
+    print(end-start)
