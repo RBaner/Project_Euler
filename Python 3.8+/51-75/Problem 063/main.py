ac74d6f3b96a1f5ba7cb70b5b74fa584783f597b
Rohit B <rbaner@rbaner.com>
2022-08-29 14:08:38 -0400

    python p63, not working

--- Python 3.8+/51-75/Problem 063/main.py
+++ Python 3.8+/51-75/Problem 063/main.py
@@ -1,8 +1,17 @@
 from math import log10
 
-
 def main()->int:
-    while int(log10(num))+1
+    num = 1
+    power = 1
+    match = set()
+    while True:
+        while int(log10((num+1)**power))+1 <= power:
+            if int(log10(num**power))+1 == power:
+                match.add(num)
+            if int(log10(2**power))+1 > power:
+                return(len(match))
+            num+=1
+        power+=1
 
 if __name__=="__main__":
     from time import perf_counter
