f193c256a52cba5963c8704bc406ad0a73026d10
Rohit B <rbaner@rbaner.com>
2022-08-28 23:02:17 -0400

    python p62, in progress

---
+++ Python 3.8+/51-75/Problem 063/main.py
@@ -0,0 +1,11 @@
+from math import log10
+
+
+def main()->int:
+    while int(log10(num))+1
+
+if __name__=="__main__":
+    from time import perf_counter
+    start = perf_counter()
+    print(main())
+    print(perf_counter()-start)
